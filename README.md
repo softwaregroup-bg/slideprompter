2c06f0d410f1fbd989178fbcee17bceb9c358eb2
Kalin Krustev <kalin.krustev@softwaregroup.com>
2021-02-17 13:21:38 +0200

    feat: initial version

---
+++ README.md
@@ -0,0 +1,55 @@
+# Slideprompter
+
+Presents Google Slides and narrates them with synthesized voice.
+
+## Usage
+
+```sh
+npx slideprompter
+```
+
+## Configuration
+
+Slideprompter will look at the current directory for a file named
+`slides.js` or `slides.json` with the following settings:
+
+- **chrome**: path to the chrome browser executable
+- **start**: URL of the start slide
+- **script**: the text to speak.
+
+Due to limitations of the speech synthesis API, long texts need to be broken down
+and separated by empty lines.
+
+Use the text `'Next'` to advance to the next slide or animation.
+
+Here is an example:
+
+```js
+module.exports = {
+    chrome: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+    start: 'https://docs.google.com/presentation/...',
+    script: `
+    Welcome this is the first slide.
+
+    Next
+
+    This is the second slide
+
+    Next
+
+    This is animation in the second slide.
+
+    If the text is very long, it must be broken by including empty lines.
+
+    Next
+
+    This is the last slide.
+    Good bye!
+    `
+};
+```
+
+## Recording the presentation
+
+To record the presentation use your favorite screen recorder, for example
+use `WIN+R` under Windows.
---
+++ index.js
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-console */
+const puppeteer = require('puppeteer-core');
+const path = require('path');
+const config = require(path.join(process.cwd(), 'slides'));
+
+function speak(what, voice) {
+    const synth = window.speechSynthesis;
+    if (synth.speaking) {
+        console.error('speechSynthesis.speaking');
+        return;
+    }
+    let done, error;
+    function next(current) {
+        const item = what[current];
+
+        const callnext = (delay = 1000) => function() {
+            if (current + 1 >= what.length) {
+                done(true);
+            } else {
+                setTimeout(() => next(current + 1), delay);
+            }
+        };
+
+        switch (typeof item) {
+            case 'string': {
+                const utterThis = new SpeechSynthesisUtterance(what[current]);
+                utterThis.onend = callnext();
+                utterThis.onerror = error;
+                utterThis.voice = window.speechSynthesis.getVoices().find(({name}) => name === (voice || 'Google UK English Female'));
+                utterThis.pitch = 1;
+                utterThis.rate = 1;
+                synth.speak(utterThis);
+                break;
+            }
+            case 'number':
+                callnext(item)();
+                break;
+            default:
+                done(true);
+        }
+    }
+    return new Promise((resolve, reject) => {
+        done = resolve;
+        error = reject;
+        next(0);
+    });
+}
+
+const delay = time => new Promise((resolve, reject) => setTimeout(resolve, time));
+
+(async() => {
+    const browser = await puppeteer.launch({
+        executablePath: config.chrome,
+        headless: false,
+        defaultViewport: null,
+        ignoreDefaultArgs: [
+            '--enable-automation',
+            '--enable-blink-features=IdleDetection',
+            '--disable-component-extensions-with-background-pages',
+            '--disable-features=Translate'
+        ],
+        args: [
+            '--start-fullscreen'
+        ]
+    });
+    const page = (await browser.pages())[0];
+    await page.goto(config.start);
+    await page.keyboard.down('ControlLeft');
+    await page.keyboard.press('F5');
+    await page.keyboard.up('ControlLeft');
+    const context = await page.mainFrame().executionContext();
+    await context.evaluate(speak, ['5', 1000], config.voice);
+    await context.evaluate(speak, ['4', 1000], config.voice);
+    await context.evaluate(speak, ['3', 1000], config.voice);
+    await context.evaluate(speak, ['2', 1000], config.voice);
+    await context.evaluate(speak, ['1', 1000], config.voice);
+    for (const item of config.script.split(/\n\n|\r\n\r\n/g).map(text => text.trim())) {
+        if (item.toLowerCase() === 'next') {
+            await delay(1000);
+            await page.keyboard.press('Space');
+        } else {
+            console.log(item);
+            await context.evaluate(speak, [item], config.voice);
+        }
+    }
+})();
